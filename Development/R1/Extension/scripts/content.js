55810cb9a1a0841b1d772dc029bc35a613b009d0
Maya <maya.arguell@gmail.com>
2018-02-17 16:12:20 -0500

    Skinned inline sizing modal

--- Development/R1/Extension/scripts/content.js
+++ Development/R1/Extension/scripts/content.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", function (event) { 
-    document.getElementbyId('fit-sizes').style.color('red');
+// content.js
+console.log("Hello from your Chrome extension!");
+
+var offset;
+var x;
+var y;
+
+if ($('#fit-sizes').length) {
+    $.get(chrome.extension.getURL('../inline.html'), function(data) {
+        //$(data).appendTo('body');
+        // Or if you're using jQuery 1.8+:
+         $($.parseHTML(data)).appendTo('body');
+
+        offset = $('#fit-sizes').offset();
+        x = offset.left + $('#fit-sizes').width() - $('sizeup-card').width();
+        y = offset.top;
+
+        console.log(x, ", ", y);
+        $('sizeup-card').css('left', x).css('top', y+44);
+    });
+} else {
+    console.log("not a product page");
+}
+
+$('#sizeup-card-dismiss').on('click', function() {
+    $('sizeup-card').hide();
 });
